6db67565a7c2440aa7a72369559f251e00849582
Waleed Khalid <waleed@codexsquared.com>
2022-02-24 16:55:33 +0500

    Update code Waleed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import React from "react";
-import { BrowserRouter as Router, Link, Routes, Route, Navigate } from "react-router-dom";
-import { Home, About, Contact } from "./Components";
+import React, { Suspense, lazy } from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+// import { Home, About, Contact } from "./Components";
+const { Home }= lazy(() => import("./Components/Home"));
+const { About }= lazy(() => import("./Components/About"));
+const { Contact }= lazy(() => import("./Components/Contact"));
 function App() {
   return (
-    <React.Fragment>
-      <Router>
-        <ul className="App-header">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About Us</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact Us</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path="about" element={<About />}></Route>
-          <Route path="contact" element={<Contact />}></Route>
-          <Route path="home" element={<Home />}>
-            <Route path="#info" element={<Home type="Info" />}></Route>
-            <Route path="#projects" element={<Home type="Projects" />}></Route>
-          </Route>
-          <Route exact path="/" element={<Navigate replace to="/home" />}></Route>
-        </Routes>
-      </Router>
-    </React.Fragment>
+      <React.Fragment>
+        <Router>
+          <ul className="App-header">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path="about" element={ <Suspense fallback={<div>Loading...</div>}><About /></Suspense>}></Route>
+            <Route path="contact" element={<Suspense fallback={<div>Loading...</div>}><Contact /></Suspense>}></Route>
+            <Route path="home" element={<Suspense fallback={<div>Loading...</div>}><Home /></Suspense>}>
+              <Route path="#info" element={<Suspense fallback={<div>Loading...</div>}><Home type="Info" /></Suspense>}></Route>
+              <Route
+                path="#projects"
+                element={<Suspense fallback={<div>Loading...</div>}><Home type="Projects" /></Suspense>}
+              ></Route>
+            </Route>
+            <Route
+              exact
+              path="/"
+              element={<Navigate replace to="/home" />}
+            ></Route>
+          </Routes>
+        </Router>
+      </React.Fragment>
   );
 }
 
